2ac63fba44ce6632f95b84d418bb6c32006daed4
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-09-07 10:30:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if($method == 'POST')
 	$com = strtolower($com);
 	
 		
-	if ($com == 'amountsold' or $com == 'margin' or $com == 'qtysold' or $com=='shoplist' or $com=='listfamily' or $com=='listcategory' or $com=='listarticle') 
+	if ($com == 'listcity' or $com == 'liststates' or $com == 'amountsold' or $com == 'margin' or $com == 'qtysold' or $com=='shoplist' or $com=='listfamily' or $com=='listcategory' or $com=='listarticle') 
 	{
 		if(isset($json->queryResult->parameters->STATE))
 		{	$STATE= $json->queryResult->parameters->STATE; } else {$STATE = '0';}
@@ -94,7 +94,7 @@ if($method == 'POST')
 		{
 			$ARTICLE = 'ALL';
 		}
-		$json_url = "http://74.201.240.43:8000/ChatBot/Sample_chatbot/EFASHION_TEST2.xsjs?command=$com&STATE=$STATE&CITY=$CITY&SHOPNAME=$SHOPNAME&YR=$YR&QTR=$QTR&MTH=$MTH&FAMILY=$FAMILY&CATEGORY=$CATEGORY&ARTICLE=$ARTICLE";		
+		$json_url = "http://74.201.240.43:8000/ChatBot/Sample_chatbot/EFASHION_DEV.xsjs?command=$com&STATE=$STATE&CITY=$CITY&SHOPNAME=$SHOPNAME&YR=$YR&QTR=$QTR&MTH=$MTH&FAMILY=$FAMILY&CATEGORY=$CATEGORY&ARTICLE=$ARTICLE";		
 		//echo $json_url;
 		$username    = "SANYAM_K";
     		$password    = "Welcome@123";
@@ -116,53 +116,19 @@ if($method == 'POST')
 				$distext = "Total profit value is of worth $";
 			else if ($com == 'qtysold')
 				$distext = "Total quantity sold of worth $";
-			if($CITY !='0')
-			{
-				$discity = " for city ";
-			}
-			else
-			{
-				$discity = "";
-			}
-			if($FAMILY !='0')
-			{
-				$disfamily = " for family ";
-			}
-			else
-			{
-				$disfamily = "";
-			}
-            if($CATEGORY !='0')
-			{
-				$discategory = " for category ";
-			}
-			else
-			{
-				$discategory = "";
-			}
-            if($ARTICLE !='0')
-			{
-				$disarticle = " for article ";
-			}
-			else
-			{
-				$disarticle = "";
-			}
-			if($SHOPNAME != '0')
-			{
-				$disshop = " of shop ";
-			}
-			else
-			{	$disshop = "";	}
-			
-			if($YR != '0')
-			{      $disyear = " for year ";} else {$disyear = "";}
+			if($CITY !='0')	{ $discity = " for city "; } else { $discity = ""; }
+			if($STATE !='0'){ $disstate = " in state "; } else { $disstate = ""; }
+			if($FAMILY !='0'){ $disfamily = " for family "; } else {$disfamily = ""; }
+            		if($CATEGORY !='0'){ $discategory = " for category "; }	else { $discategory = ""; }
+            		if($ARTICLE !='0'){$disarticle = " for article ";} else	{ $disarticle = ""; }
+			if($SHOPNAME != '0') { $disshop = " of shop "; } else{	$disshop = "";	}
+			if($YR != '0')	{      $disyear = " for year ";} else {$disyear = "";}
 			foreach ($someobj["results"] as $value) 
 			{
-				$speech .= $distext. $value["AMOUNT"].$disshop.$value["SHOP_NAME"].$discity.$value["CITY"]." in ".$value["STATE"].$disyear.$value["YR"].$disfamily.$value["FAMILY_NAME"].$discategory.$value["CATEGORY"].$disarticle.$value["ARTICLE_LABEL"];
-				
+				$speech .= $distext. $value["AMOUNT"].$disshop.$value["SHOP_NAME"].$discity.$value["CITY"].$disstate.$value["STATE"].$disyear.$value["YR"].$disfamily.$value["FAMILY_NAME"].$discategory.$value["CATEGORY"].$disarticle.$value["ARTICLE_LABEL"];
 				$speech .= "\r\n";
 			 }
+			if($speech != "") { $speech .= "I can drill down further\n";}
 		}
 		else if($com == 'shoplist')
 		{
@@ -171,6 +137,32 @@ if($method == 'POST')
 				$speech .= $value["SHOP_NAME"]." availabe in ".$value["CITY"]." in ".$value["STATE"];
 				$speech .= "\r\n";
 			 }
+		}
+		else if ($com == 'liststates')
+		{
+			$speech = "You can see values for following states";
+			$speech .= "\r\n";
+			foreach ($someobj["results"] as $value) 
+			{
+				
+				$speech .= $value["STATE"]." - ".$value["SHORT_STATE"];
+				$speech .= "\r\n";
+			}
+			$speech .= "Which would you prefer?";
+			
+		}
+		else if ($com == 'listcity')
+		{
+			$speech = "You can see values for following cities";
+			$speech .= "\r\n";
+			foreach ($someobj["results"] as $value) 
+			{
+				
+				$speech .= $value["CITY"];
+				$speech .= "\r\n";
+			}
+			$speech .= "Which would you prefer?";
+			
 		}
 		
 			
